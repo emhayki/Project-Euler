e15834036b1e31b337a79b6b260bff6229de8463
e7ayki <135982304+e7ayki@users.noreply.github.com>
2023-08-17 14:47:48 +0000

    " "

---
+++ C++/pe002.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+int main() {
+
+double sum = 0; int i = 2;
+std::vector<long> FibonacciSequence;
+
+FibonacciSequence.push_back(0);
+FibonacciSequence.push_back(1);
+
+while (true){
+    FibonacciSequence.push_back(FibonacciSequence[i-1] + FibonacciSequence[i-2]);
+    if (FibonacciSequence[i] > 4000000) break;
+    if(FibonacciSequence[i] % 2 == 0) sum += FibonacciSequence[i];
+    i++;
+}
+
+std::cout << std::fixed << std::setprecision(0) << sum << std::endl;
+
+
+    return 0;
+}
