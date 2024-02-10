5d29364dbb3f5bc32418ed22df0d4cc88af127d5
emhayki <135982304+emhayki@users.noreply.github.com>
2024-02-10 19:52:33 +0000

    No commit message

---
+++ Python/pe007.py
@@ -0,0 +1,13 @@
+
+import os
+os.system('clear')
+import numpy as np
+from sympy import prime
+
+# Problem 7
+# =========
+#    By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+#    that the 6th prime is 13.
+#    What is the 10 001st prime number?
+
+print(prime(10001))
